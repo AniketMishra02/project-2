e346fcabed6fd2d65a6043d10cb5934ed2557d8c
Aniket <aniket02mishra@gmail.com>
2023-02-16 11:25:18 +0530

    Use Fetch API from News API in News.js

--- src/component/News.js
+++ src/component/News.js
@@ -275,6 +275,13 @@ export class News extends Component {
             loading: false
         }
     }
+    async componentDidMount(){
+        let url ="https://newsapi.org/v2/everything?q=apple&from=2023-02-15&to=2023-02-15&sortBy=popularity&apiKey=9ac1098f2993452c86320b8049e73465"
+        let data = await fetch(url);
+        let parseData = await data.json()
+        console.log(parseData);
+        this.setState({articles: parseData.articles})
+    }
   render() {
     return (
       <div className="container my-3">
@@ -282,7 +289,7 @@ export class News extends Component {
         <div className="row">
         {this.state.articles.map((element)=>{
             return <div className="col-md-3" key={element.url}>
-            <NewsItem  title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url}/>
+            <NewsItem  title={element.title?element.title.slice(0,30):""} description={element.description?element.description.slice(0,50):""} imgUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
         })}
         </div>
